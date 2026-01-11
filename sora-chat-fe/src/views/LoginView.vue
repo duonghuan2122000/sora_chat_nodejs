1caa8bb521fd505995813829626eaaf2949dd20a
Jocelyn <duong.huan2122000@gmail.com>
2026-01-11 21:22:33 +0700

    #feat: Xây dựng chức năng đăng nhập
    - Cập nhật giao diện đăng nhập

--- sora-chat-fe/src/views/LoginView.vue
+++ sora-chat-fe/src/views/LoginView.vue
@@ -4,50 +4,59 @@
 -->
 <template>
   <div class="LoginView flex flex-col items-center">
-    <Card class="min-w-[400px] max-w-[800px] mt-[50px]">
-      <CardHeader>
-        <CardTitle> Đăng nhập </CardTitle>
-      </CardHeader>
-      <CardContent>
-        <form class="flex flex-col gap-4" @submit.prevent="handleLogin">
-          <VeeField v-slot="{ field, errors }" name="username">
-            <Field>
-              <FieldLabel>Tên đăng nhập</FieldLabel>
-              <Input
-                v-bind="field"
-                placeholder="Nhập tên đăng nhập"
-                autoFocus
-                :aria-invalid="!!errors.length"
-              />
-              <FieldError v-if="errors.length" :errors="errors" />
-            </Field>
-          </VeeField>
-          <VeeField v-slot="{ field, errors }" name="password">
-            <Field>
-              <FieldLabel>Tên mật khẩu</FieldLabel>
-              <Input
-                v-bind="field"
-                placeholder="Nhập mật khẩu"
-                type="password"
-                :aria-invalid="!!errors.length"
-              />
-              <FieldError v-if="errors.length" :errors="errors" />
-            </Field>
-          </VeeField>
-          <div>
-            <Button type="submit">Đăng nhập</Button>
+    <ElCard class="min-w-[400px] max-w-[800px] mt-[50px]">
+      <template #header>
+        <h2>Đăng nhập</h2>
+      </template>
+      <form class="flex flex-col gap-4" @submit.prevent="handleLogin">
+        <VeeField v-slot="{ value, handleChange, errors }" name="username">
+          <div class="flex flex-col gap-2">
+            <label
+              class="items-center text-sm font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50 group/field-label peer/field-label flex w-fit gap-2 leading-snug group-data-[disabled=true]/field:opacity-50 has-[>[data-slot=field]]:w-full has-[>[data-slot=field]]:flex-col has-[>[data-slot=field]]:rounded-md has-[>[data-slot=field]]:border [&>*]:data-[slot=field]:p-4 has-data-[state=checked]:bg-primary/5 has-data-[state=checked]:border-primary dark:has-data-[state=checked]:bg-primary/10"
+              for="username-input"
+              >Tên đăng nhập</label
+            >
+            <ElInput
+              id="username-input"
+              :modelValue="value"
+              @update:modelValue="handleChange"
+              placeholder="Nhập tên đăng nhập"
+              autoFocus
+              :aria-invalid="!!errors.length"
+            />
+            <ul v-if="errors" class="text-red-500 text-sm list-disc ml-4">
+              <li v-for="err in errors">{{ err }}</li>
+            </ul>
           </div>
-        </form>
-      </CardContent>
-    </Card>
+        </VeeField>
+        <VeeField v-slot="{ value, handleChange, errors }" name="password">
+          <div class="flex flex-col gap-2">
+            <label
+              for="password-input"
+              class="items-center text-sm font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50 group/field-label peer/field-label flex w-fit gap-2 leading-snug group-data-[disabled=true]/field:opacity-50 has-[>[data-slot=field]]:w-full has-[>[data-slot=field]]:flex-col has-[>[data-slot=field]]:rounded-md has-[>[data-slot=field]]:border [&>*]:data-[slot=field]:p-4 has-data-[state=checked]:bg-primary/5 has-data-[state=checked]:border-primary dark:has-data-[state=checked]:bg-primary/10"
+              >Tên mật khẩu</label
+            >
+            <ElInput
+              :modelValue="value"
+              @update:modelValue="handleChange"
+              placeholder="Nhập mật khẩu"
+              type="password"
+              :aria-invalid="!!errors.length"
+            />
+            <ul v-if="errors" class="text-red-500 text-sm list-disc ml-4">
+              <li v-for="err in errors">{{ err }}</li>
+            </ul>
+          </div>
+        </VeeField>
+        <div>
+          <ElButton type="primary" native-type="submit">Đăng nhập</ElButton>
+        </div>
+      </form>
+    </ElCard>
   </div>
 </template>
 
 <script setup>
-import { Button } from "@/components/ui/button";
-import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
-import { Field, FieldLabel, FieldError } from "@/components/ui/field";
-import { Input } from "@/components/ui/input";
 import { toTypedSchema } from "@vee-validate/zod";
 import { onMounted } from "vue";
 import { z } from "zod";
@@ -55,6 +64,9 @@ import { useForm, Field as VeeField } from "vee-validate";
 import { userApi } from "@/apis/users/user.api";
 import { useRouter } from "vue-router";
 import { RouterName } from "@/commons/const.common";
+
+// Component
+import { ElInput, ElButton, ElCard } from "element-plus";
 
 const router = useRouter();
 
